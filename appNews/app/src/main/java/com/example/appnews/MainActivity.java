55705e7221de31e4bc2cb5f19f36f4f285d8979c
Davi Barbosa Vieira Bispo <davib.g767@gmail.com>
2022-09-14 16:08:09 -0300

    finish screens

--- appNews/app/src/main/java/com/example/appnews/MainActivity.java
+++ appNews/app/src/main/java/com/example/appnews/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btnProximo1;
+    Button btnAnterior;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnProximo1 = findViewById(R.id.btnProximo1);
+        btnAnterior = findViewById(R.id.btnanterior);
 
         btnProximo1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(segundaTela);
             }
         });
+
+        btnAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ultimaTela = new Intent(getApplicationContext(), QuintaTela.class);
+                startActivity(ultimaTela);
+            }
+        });
+
     }
 }
--- appNews/app/src/main/java/com/example/appnews/QuintaTela.java
+++ appNews/app/src/main/java/com/example/appnews/QuintaTela.java
@@ -1,14 +1,40 @@
 package com.example.appnews;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuintaTela extends AppCompatActivity {
+    Button btnProximo;
+    Button btnAnterior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quinta_tela);
+
+        btnProximo = findViewById(R.id.btnProximo5);
+        btnAnterior = findViewById(R.id.btnAnterior5);
+
+        btnProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent primeiraTela = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(primeiraTela);
+            }
+        });
+
+        btnAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quartaTela = new Intent(getApplicationContext(),QuartaTela.class);
+                startActivity(quartaTela);
+            }
+        });
+
+
     }
+
 }
